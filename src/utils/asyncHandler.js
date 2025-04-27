e3e76d310980902c407891d4d3e7327e14bc2ba1
Ineffable-Rock <krishkr8691@gmail.com>
2025-04-27 13:51:27 +0530

    Controller and route setup

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,8 +1,7 @@
 const asyncHandler = (requesHandler) => {
-    (req,res,next) => {
+    return (req,res,next) => {
         Promise.resolve(requesHandler(req,res,next)).
         catch((err) => next(err))
-
     }
 }
 
